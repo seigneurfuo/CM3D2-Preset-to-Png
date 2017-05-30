045d9f16e453f596196f40229c01530bcaff31e4
seigneurfuo <seigneurfuo@users.noreply.github.com>
2017-05-30 14:06:02 +0200

    Add English comments in the code

--- preset2png.py
+++ preset2png.py
@@ -1,30 +1,42 @@
 # seigneurfuo
-# 30 avril 2017
-# v0.01
+# 30 May 2017
+# v0.02
 
 import sys
 
-dossier = sys.argv[1]
-def preset2png(INFILENAME):
-    fo = open(INFILENAME, "rb") # Fichier d'entrée
-    fi = open("%s.png" %INFILENAME, "wb") # Fichier de sortie
+def preset2png(inputFilename):
+    """Extracting PNG data from a CM3D2 preset file"""
+    
+    # Input file
+    inputFile = open(inputFilename, "rb")
+    
+    # Ouput file
+    outputFilename = "%s.png" %inputFilename
+    outputFile = open(outputFilename, "wb")
 
-    fo.seek(25, 0) # Commencement à la position
+    # Start reading all the input file from the 25th position
+    inputFile.seek(25, 0)
 
-    content = fo.read() # Lecture de tout le document
+    content = inputFile.read()
+    
+    # Split where the PNG ends
     contentSplit = content.split(b"\x49\x45\x4e\x44\xae\x42")
+    
+    # And get the first part
     pngData = contentSplit[0]
 
-
-    fi.write(pngData)
-    fi.write(b"\x49\x45\x4e\x44\xae\x42")
+    # Writing ouput file
+    outputFile.write(pngData)
+    outputFile.write(b"\x49\x45\x4e\x44\xae\x42")
         
-    fo.close()
-    fi.close()
+    # Closing files
+    inputFile.close()
+    outputFile.close()
 
 if __name__ == "__main__":
-    import glob, os
-    
-    for file in glob.glob("%s/*.preset" %dossier):
-        print(file)
-        preset2png(file)
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+        preset2png(filename)
+        
+    else:
+        print("Usage: preset2png.py <preset file>")
